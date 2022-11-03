2909e38dcaa2d7242d0ab98b8dc194b6ff0c6152
akanksha0911 <rawatakanksha2020@gmail.com>
2022-11-03 12:27:18 -0700

    First commit

---
+++ 1-intro.js
@@ -0,0 +1,20 @@
+const number = 10;
+
+if(number<12){
+    console.log('small number')
+}
+else{
+    console.log('big number')
+}
+
+console.log('First Code')
+
+////////////////////////////////////////
+
+const sayHi = (name) => {
+    console.log(`Hello ${name}, how are you ?`)
+}
+
+const peter = 'Peter'
+sayHi(peter)
+sayHi('Akanksha')
---
+++ 2-names.js
@@ -0,0 +1,5 @@
+const john = 'john';
+const peter = 'peter';
+const notSharing = 'Notsharing'
+
+module.exports = {john ,peter}
